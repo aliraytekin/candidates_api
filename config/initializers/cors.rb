022752bc3004f6e14f3b818b5d11cea2802d4957
Ali Aytekin <aliraytekin@gmail.com>
2025-08-20 16:06:43 +0300

    Fix CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://candidates-api-restless-fire-9611.fly.dev"
+    origins(
+      'http://localhost:3000',
+      'https://candidates-dashboard.vercel.app'
+    )
 
     resource "*",
       headers: :any,
